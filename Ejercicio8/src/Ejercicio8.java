48547b7d4dc14e635e77feae66d90a5ec9d114b8
Brarias <60743620+Brarias@users.noreply.github.com>
2022-06-17 23:43:19 -0300

    resolución ejercicio de la unidad 8

---
+++ Ejercicio8/src/Ejercicio8.java
@@ -0,0 +1,14 @@
+public class Ejercicio8 {
+    public static void main(String[] args) {
+
+        Persona persona = new Persona();
+        persona.setEdad(30);
+        persona.setNombre("Eustaquio");
+        persona.setTelefono("1156789021");
+
+        System.out.println(persona.getEdad());
+        System.out.println(persona.getNombre());
+        System.out.println(persona.getTelefono());
+
+    }
+}
