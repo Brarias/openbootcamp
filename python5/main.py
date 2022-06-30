8a9498a0fc9d447e449c02bfe49ae15243884c21
Brarias <60743620+Brarias@users.noreply.github.com>
2022-06-30 07:51:21 -0300

    ejercicio 1 de tema 5 python

---
+++ python5/main.py
@@ -0,0 +1,9 @@
+#Escribe una función que calcule el área de un triángulo, recibiendo la altura y la base como parámetros y
+# otra función que calcule el área de un círculo recibiendo el radio del mismo.
+
+def areaTriangulo(base, altura):
+    return (base* altura) / 2
+
+
+resultado = areaTriangulo(3, 4)
+print(resultado)
